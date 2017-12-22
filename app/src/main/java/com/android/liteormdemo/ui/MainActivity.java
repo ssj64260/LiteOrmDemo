62b8e1c4489994bfe4d802aa9f50016d0c6ac4b3
chenxuebin <799536767@qq.com>
2017-12-22 12:01:26 +0800

    #新建项目

---
+++ app/src/main/java/com/android/liteormdemo/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.android.liteormdemo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.liteormdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
